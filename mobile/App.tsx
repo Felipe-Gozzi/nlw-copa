cb49724e6c494dd9b9f590353d41bef4c46ee221
Felipe Gozzi <felipegozzi16@gmail.com>
2022-11-04 00:31:52 -0300

    Class 04 - BackEnd

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,7 +9,7 @@ import {
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import { Loading } from "./src/components/Loading";
-import { Pools } from "./src/screens/Pools";
+import { SignIn } from "./src/screens/SignIn";
 
 import { THEME } from "./src/styles/theme";
 
@@ -28,7 +28,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <SignIn /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -15,7 +15,7 @@ export function SignIn() {
 
       <Button
         type="SECONDARY"
-        title="ENTRfdfAR COM O GOOGLE"
+        title="ENTRAR COM O GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={12}
         onPress={signIn}
