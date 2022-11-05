b69ce8bb2f30ccd0bfa042cb1d6a4559d0a8f8c8
Felipe Gozzi <felipegozzi16@gmail.com>
2022-11-05 02:17:53 -0300

    Class 05 - Mobile Final

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -27,8 +27,7 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
   const [isUserLoading, setIsUserLoading] = useState(false);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId:
-      "140154076861-df944f06vurbuakff1erb4f3eegct5ie.apps.googleusercontent.com",
+    clientId: process.env.CLIENT_ID,
     redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
     scopes: ["profile", "email"],
   });
